df78012b5a5f41b2173f6237ab8d9690da538954
mbadalian <supreme.opp@gmail.com>
2023-05-03 09:34:25 +0300

    add breed search, controls for images amount, remove legacy files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
 import React, {useEffect, useState} from "react";
 import CatsDataService from "./services/service";
 import {
-    Paper,
-    Table,
-    TableRow,
-    TableCell,
-    TableBody,
-    TableContainer,
+    Autocomplete,
+    TextField,
+    Box,
+    AppBar,
+    Toolbar,
+    Typography,
     Container,
     ImageList,
     ImageListItem,
     Button,
-    Skeleton
+    ButtonGroup,
+    Skeleton,
+    Fab
 } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import RefreshRoundedIcon from '@mui/icons-material/RefreshRounded';
 import {NotificationManager} from 'react-notifications';
-import './App.css';
+
+const fab = {
+  margin: 0,
+  top: 'auto',
+  right: 20,
+  bottom: 20,
+  left: 'auto',
+  position: 'fixed',
+};
+const vertical = {
+  margin: 0,
+  top: 'auto',
+  left: 20,
+  bottom: 20,
+  right: 'auto',
+  position: 'fixed',
+};
 
 const CatsList = () => {
   const [cats, setCats] = useState([]);
+  const [breeds, setBreeds] = useState();
+  const [selectedBreed, setSelectedBreed] = useState();
+  const [amount, setAmount] = useState(9);
 
   useEffect(() => {
     retrieveCats();
+    retrieveBreeds();
   }, []);
 
   const retrieveCats = () => {
-    CatsDataService.getByAmount(9)
+    CatsDataService.getByAmount(amount)
       .then(response => {
         setCats(response.data);
       })
@@ -34,32 +58,101 @@ const CatsList = () => {
       });
   };
 
+  const retrieveBreeds = () => {
+    CatsDataService.getBreeds()
+      .then(response => {
+        setBreeds(response.data);
+      })
+      .catch(error => {
+        NotificationManager.error(`$error`, "Error obtaining breeds");
+      });
+  };
+
+  const retrieveById = () => {
+    CatsDataService.getByBreed(selectedBreed, amount)
+      .then(response => {
+        setCats(response.data);
+      })
+      .catch(error => {
+        NotificationManager.error(`$error, "Error obtaining by breed`)
+
+      });
+  };
+
+  const handleRefresh = () => {
+    selectedBreed ? retrieveById() : retrieveCats()
+  }
+
+  const handleIncrement = () => {
+    setAmount(amount + 1);
+  };
+
+  const handleDecrement = () => {
+    setAmount(amount - 1);
+  };
+
   return (
     <Container maxWidth="md">
-      <ImageList>
-        {cats.map((cat) => (
-          <ImageListItem key={cat.url}>
-            <img
-            src={`${cat.url}?w=164&h=164&fit=crop&auto=format`}
-            srcSet={`${cat.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            alt={cat.id}
-            loading="lazy"
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
-      <Button onClick={retrieveCats} variant="contained" endIcon={<RefreshRoundedIcon />}>Refresh</Button>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            OnlyCats
+          </Typography>
+          <Autocomplete
+            sx={{ml: 2, width: 300}}
+            disablePortal
+            id="combo-box"
+            options={breeds?.map(option => ({ id: option.id, label: option.name}))}
+            onChange={(e, value) => setSelectedBreed(value?.id)}
+            renderInput={(params) => <TextField {...params} label="Breed Search" />}
+          />
+          <Box sx={{ flexGrow: 1 }} />
+        </Toolbar>
+      </AppBar>
+    </Box>
+        <ImageList>
+          {cats.map((cat) => (
+            <ImageListItemContainer key={cat.ids} url={cat.url} />
+          ))}
+        </ImageList>
+      <Fab style={fab} color="primary" aria-label="add" onClick={handleRefresh}>
+        <RefreshRoundedIcon />
+      </Fab>
+      <ButtonGroup style={vertical} variant="contained" orientation="vertical" aria-label="outlined primary button group">
+        <Button onClick={handleIncrement}>
+          <AddIcon />
+        </Button>
+        <Button>
+          <Typography>{amount}</Typography>
+        </Button>
+        <Button onClick={handleDecrement}>
+          <RemoveIcon />
+        </Button>
+      </ButtonGroup>
     </Container>
   );
 };
 
-// const ImageListItemContainer = (props) => {
-//   return (
-//       <>
-//         loadingContext && <Skeleton />
-//         <ImageListItem {...props} onload={() => setLoadingContext(false)}>
-//         </ImageListItem>
-//       </>
-// )};
+function ImageListItemContainer({ props, url }) {
+  const [loading, setLoading] = useState(true);
+  return (
+    <>
+      <Skeleton sx={{display: loading ? "block" : "none" }} variant="rectangular"/>
+      <ImageListItem sx={{display: loading ? "none" : "block"}} {...props}>
+        <img
+          src={`${url}`}
+          srcSet={`${url}`}
+          onLoad={() => setLoading(false)}
+          alt=""
+        />
+      </ImageListItem>
+    </>
+)}
 
 export default CatsList;
--- src/services/service.js
+++ src/services/service.js
@@ -3,6 +3,12 @@ import https from "../helpers/https";
 class CatsDataService {
     getByAmount(amount) {
         return https.get(`/images/search?limit=${amount}`)
+    };
+    getBreeds() {
+        return https.get(`/breeds`)
+    };
+    getByBreed(breed, amount=1) {
+        return https.get(`/images/search?limit=${amount}&breed_ids=${breed}`)
     }
 }
 export default new CatsDataService();
